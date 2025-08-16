9544eb432b5bd16e092fdfaf4ff0e5ffe629d445
Lucas Costa <lucsyntaxdev@gmail.com>
2025-08-16 17:05:59 -0300

    feat: ensure product quantity can be decreased

---
+++ src/actions/decrease-cart-product-quantity/schema.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const decreaseCartProductQuantitySchema = z.object({
+  cartItemId: z.uuid(),
+});
+
+
+export type DecreaseCartProductQuantitySchema = z.infer<typeof decreaseCartProductQuantitySchema>
